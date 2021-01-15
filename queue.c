105184f31fd820bb1a2b32b6847b67d2f52f33a7
Emir <emirbuckun@hotmail.com>
2021-01-16 01:03:51 +0300

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,116 @@
+// A C program to demonstrate linked list based implementation of queue 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+
+// A linked list (LL) node to store a queue entry 
+struct QNode { 
+	int key; 
+	struct QNode* next; 
+}; 
+
+// The queue, front stores the front node of LL and rear stores the 
+// last node of LL 
+struct Queue { 
+	struct QNode *front, *rear; 
+}; 
+
+// A utility function to create a new linked list node. 
+struct QNode* newNode(int k) 
+{ 
+	struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode)); 
+	temp->key = k; 
+	temp->next = NULL; 
+	return temp; 
+} 
+
+// A utility function to create an empty queue 
+struct Queue* createQueue() 
+{ 
+	struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue)); 
+	q->front = q->rear = NULL; 
+	return q; 
+} 
+
+// The function to add a key k to q 
+void enQueue(struct Queue* q, int k) 
+{ 
+	// Create a new LL node 
+	struct QNode* temp = newNode(k); 
+
+	// If queue is empty, then new node is front and rear both 
+	if (q->rear == NULL) { 
+		q->front = q->rear = temp; 
+		return; 
+	} 
+
+	// Add the new node at the end of queue and change rear 
+	q->rear->next = temp; 
+	q->rear = temp; 
+}
+
+// Function to remove a key from given queue q 
+void deQueue(struct Queue* q) 
+{ 
+    // If queue is empty, return NULL. 
+    if (q->front == NULL) 
+        return; 
+  
+    // Store previous front and move front one node ahead 
+    struct QNode* temp = q->front; 
+  
+    q->front = q->front->next; 
+  
+    // If front becomes NULL, then change rear also as NULL 
+    if (q->front == NULL) 
+        q->rear = NULL; 
+  
+    free(temp); 
+} 
+
+// Check for prime
+int CheckPrime (int num) 
+{
+    int flag = 1;
+    
+    if (num == 1 || num == 0) {
+        flag = 0;
+        return flag; 
+    }
+    
+    for (int j = 2; j <= num / 2; ++j) 
+    {
+    	if (num % j == 0) { 
+		    flag = 0; 
+		    break; 
+	    } 
+	}
+	return flag;
+}
+
+// Driver Program to test anove functions 
+int main() 
+{ 
+	struct Queue* q = createQueue(); 
+    int lower = 1, upper = 100, counter = 5;
+    srand(time(0)); 
+    
+    printf("Queue: ");
+    while (counter != 0)
+    {
+        int num = (rand() % (upper - lower + 1)) + lower;
+        if (CheckPrime(num) == 1){
+            enQueue(q, num);
+            counter--;
+            printf("%d", num);
+            if (counter != 0)
+                printf("->");
+        }
+    }
+    printf("\n");
+    
+    deQueue(q); 
+    deQueue(q);
+	printf("ortadaki Eleman: %d", q->front->key); 
+	return 0; 
+} 
